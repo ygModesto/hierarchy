6a668d945cdf8e209c7f3205810c02a11b2c5732
Yago Modesto <modesto@ensilabs.com>
2019-09-02 19:02:08 +0200

    bug: Ambiguous mapping methods found for mapping property...

--- src/main/java/com/mapstruct/issue/hierarchy/mapper/UserMapper.java
+++ src/main/java/com/mapstruct/issue/hierarchy/mapper/UserMapper.java
@@ -6,6 +6,7 @@ import org.mapstruct.Mapper;
 import org.mapstruct.factory.Mappers;
 
 import com.mapstruct.issue.hierarchy.model.User;
+import com.mapstruct.issue.hierarchy.to.UserBaseTO;
 import com.mapstruct.issue.hierarchy.to.UserTO;
 
 @Mapper
@@ -13,6 +14,8 @@ public interface UserMapper {
 
 	UserMapper INSTANCE = Mappers.getMapper(UserMapper.class);
 	
+	UserBaseTO userToUserBaseTO(User user);
+	Collection<UserTO> userToUserBaseTO(Collection<User> users);	
 	UserTO userToUserTO(User user);
 	Collection<UserTO> userToUserTO(Collection<User> users);
 	
